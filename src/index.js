1515938fe2fc9108df40cdb19c83768355fb0c5f
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-05-22 22:33:53 +0800

    An Intro to React: 8 - ES6 Import Statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
+
 // create a new component, this component should prouce some HTML (create)
 const App = () => {
   return <div>Hi</div>;
 }
 
-
 // Take this component's generated HTML and put it on the page (in the DOM) (show)
+React.render(App);
