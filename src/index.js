25e5b2ec7f6587080d4e3c393af4ef519bd2e21e
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-05-22 22:24:01 +0800

    An Intro to React: 6 - A Taste of JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// create a new component, this component should prouce some HTML (create)
+const App = () => {
+  return <div>Hi</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM) (show)
