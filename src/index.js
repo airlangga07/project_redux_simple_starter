c53908217681692c363db60008ff49713465380c
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-05-23 21:21:30 +0800

    An Intro to React: 13 - Youtube Search API Signup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const YOUTUBE_API_KEY = 'AIzaSyAMeLBKWEM4-EiIy5WmdPGhpJ-tSBPu-TE';
+
 // create a new component, this component should prouce some HTML (create)
 const App = () => {
   return <div>Hi</div>;
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM) (show)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
