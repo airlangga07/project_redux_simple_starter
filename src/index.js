0dbacdb508081c117fff4d0674be24deba49e0ff
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-05-23 21:31:45 +0800

    An Intro to React: 14 - Export Statements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const YOUTUBE_API_KEY = 'AIzaSyAMeLBKWEM4-EiIy5WmdPGhpJ-tSBPu-TE';
 
 // create a new component, this component should prouce some HTML (create)
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
