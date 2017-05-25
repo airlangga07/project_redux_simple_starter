d028d73a40b82f53770e5cfd76cc9076652bfd55
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-05-25 21:16:38 +0800

    Ajax Req with React: 21 - Youtube Search Response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyAMeLBKWEM4-EiIy5WmdPGhpJ-tSBPu-TE';
+
+YTSearch({
+  key: YOUTUBE_API_KEY,
+  term: 'surfboards'
+}, data => {
+  console.log(data);
+});
 
 // create a new component, this component should prouce some HTML (create)
 const App = () => {
